6a5b0c1e8666e41fb95b36c5e2fdae964a41c4aa
mcauliffek4298 <kathryn@peerlift.org>
2018-06-19 15:06:18 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,4 +1,4 @@
-def greeting (local_greeting,your_name,my_name,time_of_day)
+def greeting (local_greeting="Hello",your_name,my_name,time_of_day)
 puts "#{local_greeting} #{your_name}, I am #{my_name} and the time is #{time_of_day}"
 end
-greeting("Hey there", "Beyonce","Kathryn", "9:56 AM")
+greeting("Beyonce","Kathryn", "9:56 AM")
