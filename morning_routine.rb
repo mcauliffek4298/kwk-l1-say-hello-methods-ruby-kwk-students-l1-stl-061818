7bd408ea714c5f3d9eba4329efa294754e219286
mcauliffek4298 <kathryn@peerlift.org>
2018-06-19 14:46:56 +0000

    automatically backed up by learn

---
+++ morning_routine.rb
@@ -0,0 +1,7 @@
+def morning_routine
+  puts "alarm goes off, and I press snooze"
+  puts "I lay in bed for a minute"
+  puts "I get out of bed, and wake up"
+  puts "I get dresses, and change my clothes"
+end
+morning_routine
